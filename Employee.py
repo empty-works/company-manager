aaffc13cb83fc63bfddba75c2ae29c84fc491be6
empty-works <pama.mark@gmail.com>
2020-09-08 21:57:55 +0200

    Created subclasses of the Employee class such as hourly or salaried employees.

--- Employee.py
+++ Employee.py
@@ -5,4 +5,15 @@ from abc import ABCMeta
 
 class Employee(metaclass=ABCMeta):
     pass
-    
+
+class HourlyEmployee(Employee):
+    pass
+
+class SalariedEmployee(Employee):
+    pass
+
+class Manager(Employee):
+    pass
+
+class Executive(Employee):
+    pass
