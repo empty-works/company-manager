8f120c8500074408db8a500226c31b711a8a5145
empty-works <pama.mark@gmail.com>
2020-09-11 22:04:45 +0200

    Added ID number attribute and getters in the Employee class.

--- Employee.py
+++ Employee.py
@@ -5,10 +5,23 @@ from abc import ABCMeta
 
 class Employee(metaclass=ABCMeta):
     
-    def __init__(self, name = 'N/A'):
+    def __init__(self, id_num, name = 'N/A'):
+        self.id_num = id_num
         self.name = name
         self.rate = 0.00
         self.hours_per_day = 0
+
+    def get_id(self):
+        return self.id_num
+
+    def get_name(self):
+        return self.name
+
+    def get_rate(self):
+        return self.rate
+
+    def get_hours(self):
+        return self.hours_per_day
 
     def hire(self):
         pass
