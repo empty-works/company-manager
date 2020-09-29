d0f36ba7d66987fd55cd062e495b98490c098455
empty-works <pama.mark@gmail.com>
2020-09-29 19:33:22 +0200

    Added fields to an Employee table for the database.

--- CompanyManager/MainApp/models.py
+++ CompanyManager/MainApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    firstName = models.CharField(max_length = 1000)
+    lastName = models.CharField(max_length = 1000)
+    birthdate = models.DateField(blank = True)
+    address = models.CharField(max_length = 2000)
+    email = models.EmailField(blank = True)
