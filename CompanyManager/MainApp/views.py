a00ee154cb4fedda61451c8c4dd43f9679261dbd
empty-works <pama.mark@gmail.com>
2020-09-30 19:44:04 +0200

    Set view to extract all objects from the Employee database table.

--- CompanyManager/MainApp/views.py
+++ CompanyManager/MainApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main_app/index.html')
+    employees = Employee.objects.all();
+    return render(request, 'main_app/index.html', {'employees': employees})
 
 def help(request):
     return render(request, 'main_app/help.html')
