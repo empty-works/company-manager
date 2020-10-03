5e93f7a0e319766a17d455dd49fa32f2ca5cef48
empty-works <pama.mark@gmail.com>
2020-10-03 20:30:23 +0200

    Employee html file, views and urls fixed.

--- CompanyManager/CompanyManager/urls.py
+++ CompanyManager/CompanyManager/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 app_name = "home"
 
--- CompanyManager/employees/views.py
+++ CompanyManager/employees/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Employee
 
-def index(request):
+def employees(request):
     emps = Employee.objects.all()
-    return render(request, 'employees/index.html', {'emps': emps})
+    return render(request, 'employees/employees.html', {'emps': emps})
 
 def about(request):
     return render(request, 'employees/about.html')
