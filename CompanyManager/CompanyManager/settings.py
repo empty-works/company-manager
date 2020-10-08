4e8b73d8da1fff69165bfd9427688419c652318f
empty-works <pama.mark@gmail.com>
2020-10-08 18:13:39 +0200

    Committing to fix git pull.

--- CompanyManager/CompanyManager/settings.py
+++ CompanyManager/CompanyManager/settings.py
@@ -128,3 +128,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from local_settings import *
+except ImportError:
+    print("No local file. You must be on production")
