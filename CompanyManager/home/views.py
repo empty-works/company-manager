1264e140586f1ef4d7c8506fc4c8d5f280715a75
empty-works <pama.mark@gmail.com>
2020-10-10 17:38:47 +0200

    Shows error to user if username already exists.

--- CompanyManager/home/views.py
+++ CompanyManager/home/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from django.db import IntegrityError
 
 def home(request):
     if request.method == "GET":
         return render(request, 'home/home.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
+                user.save()
+            except IntegrityError:
+                return render(request, 'home/home.html', {'form': UserCreationForm(), 'error':'Usernames must be unique.'})
         else:
             return render(request, 'home/home.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
