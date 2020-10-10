039169430ea7806216463b40cab5cb9dba15adc7
empty-works <pama.mark@gmail.com>
2020-10-10 16:30:51 +0200

    Home page will now be the log in/sign up page. Jumbotron deleted.

--- CompanyManager/home/views.py
+++ CompanyManager/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
-    return render(request, 'home/home.html')
+    return render(request, 'home/home.html', {'form': UserCreationForm()})
