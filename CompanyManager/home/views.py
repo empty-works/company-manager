fc12ab80ef7b522922cffbde169463155b28a3c8
empty-works <pama.mark@gmail.com>
2020-10-10 17:30:43 +0200

    Now shows error message if the passwords did not match.

--- CompanyManager/home/views.py
+++ CompanyManager/home/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def home(request):
     if request.method == "GET":
         return render(request, 'home/home.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.createUser(request.POST['username'], password=request.POST['password2'])
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
             user.save()
         else:
-            print("Passwords didn't match")
+            return render(request, 'home/home.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
