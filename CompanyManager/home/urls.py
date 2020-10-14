d8000705a2da82dd3ee327801a8d1ccc11df2bc8
empty-works <pama.mark@gmail.com>
2020-10-14 17:37:38 +0200

    Registered signupuser URL.

--- CompanyManager/home/urls.py
+++ CompanyManager/home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'home'
+app_name = "home"
 
 urlpatterns = [
         path('home/', views.home, name = 'home'),
-        path('logout/', views.logoutuser, name = 'logoutuser'), 
+        path('signup/', views.signupuser, name = 'signupuser'),
+        path('logout/', views.logoutuser, name = 'logoutuser'),
 ]
