97cbca8c49a2df4fcf20089733b2731930e2a7a5
empty-works <pama.mark@gmail.com>
2020-10-15 16:48:12 +0200

    Created login url and view.

--- CompanyManager/home/views.py
+++ CompanyManager/home/views.py
@@ -21,6 +21,22 @@ def signupuser(request):
                 return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Usernames must be unique.'})
         else:
             return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+
+def loginuser(request):
+    if request.method == "GET":
+        return render(request, 'home/loginuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
+                user.save()
+                login(request, user)
+                return redirect('employees:employees')
+            except IntegrityError:
+                return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Usernames must be unique.'})
+        else:
+            return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+
      
 def logoutuser(request):
     if request.method == 'POST':
