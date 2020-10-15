3bd9a01bc179769c92e80bb657e45c2a16bf4fe5
empty-works <pama.mark@gmail.com>
2020-10-15 18:32:07 +0200

    Wage and dateCreated attributes in the model now migrate properly.

--- CompanyManager/employees/models.py
+++ CompanyManager/employees/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Employee types list for the employeeType choice field.
 EMP_TYPES = {
         ("1", "Potential"),
         ("2", "Hourly"),
@@ -13,11 +14,15 @@ class Employee(models.Model):
     firstName = models.CharField(max_length = 1000)
     lastName = models.CharField(max_length = 1000)
     birthdate = models.DateField(blank = True)
-    dateCreated = models.DateTimeField(auto_now_add = True)
+    dateCreated = models.DateTimeField(auto_now_add = True, null = True) # Required so no blank=True
     dateHired = models.DateTimeField(null = True, blank = True)
     dateFired = models.DateTimeField(null = True, blank = True)
-    wage = 
-    employeeType = 
+    wage = models.DecimalField(max_digits = 8, decimal_places = 2, blank = True, null = True) 
+    employeeType = models.CharField(
+            max_length = 20,
+            choices = EMP_TYPES,
+            default = '1'
+            )
     address = models.CharField(max_length = 2000)
     email = models.EmailField(blank = True)
     picture = models.ImageField(upload_to = 'employees/images/', default = None)
