fcab7a0c1271778788069d8c22e44f1450549a19
empty-works <pama.mark@gmail.com>
2020-10-15 17:18:09 +0200

    Authentication code for login. Also checks for validity of username and password.

--- CompanyManager/home/views.py
+++ CompanyManager/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 def home(request):
     return render(request, 'home/home.html')
@@ -26,16 +26,9 @@ def loginuser(request):
     if request.method == "GET":
         return render(request, 'home/loginuser.html', {'form': AuthenticationForm()})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password2'])
-                user.save()
-                login(request, user)
-                return redirect('employees:employees')
-            except IntegrityError:
-                return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Usernames must be unique.'})
-        else:
-            return render(request, 'home/signupuser.html', {'form': UserCreationForm(), 'error':'Passwords did not match'})
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'home/loginuser.html', {'form': AuthenticationForm(), 'error':'Username and/or password are not valid'})
 
      
 def logoutuser(request):
