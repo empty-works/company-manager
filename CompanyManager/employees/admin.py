7b9211ed0579dc81045b65b8d3621ce96b6967fc
empty-works <pama.mark@gmail.com>
2020-10-15 18:37:15 +0200

    Customized Django Admin to make dateCreated attribute read only.

--- CompanyManager/employees/admin.py
+++ CompanyManager/employees/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Employee
 
-admin.site.register(Employee)
+# Used to customize Django Admin
+class EmpAdmin(admin.ModelAdmin):
+    readonly_fields = ('dateCreated',)
+
+admin.site.register(Employee, EmpAdmin)
