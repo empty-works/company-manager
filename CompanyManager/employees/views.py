661cfd5db3987c45e57ba5b0a8a4a03e14ccd3a9
empty-works <pama.mark@gmail.com>
2020-10-17 18:02:05 +0200

    Add employee form now returns an value error for bad input data on the same template instead of a django debug page.

--- CompanyManager/employees/views.py
+++ CompanyManager/employees/views.py
@@ -11,11 +11,9 @@ def addEmployee(request):
         return render(request, 'employees/addemployee.html', {'form': EmployeeForm()})
     else:
         # Essentially takes the form from GET and melds the fields into a POST thing. Awesome.
-        form = EmployeeForm(request.POST)
-        form.save() 
-        #newEmployee = form.save(commit = False)
-        # Does not allow current user to make changes for other users. However, this is not really 
-        # necessary for this app because all users are "super-users" anyway.
-        #newEmployee.user = request.user
-        #newEmployee.save()
-        return render(request, 'employees/employees.html')
+        try:
+            form = EmployeeForm(request.POST)
+            form.save() 
+            return render(request, 'employees/employees.html')
+        except ValueError:
+            return render(request, 'employees/addemployee.html', {'form': EmployeeForm(), 'error':'Bad data passed in. Try again.'})
