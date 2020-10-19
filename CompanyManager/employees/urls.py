6ff9f3802377cb2c233512dd50409ad1fc5c34e7
empty-works <pama.mark@gmail.com>
2020-10-19 17:18:08 +0200

    Created template for employee details page.

--- CompanyManager/employees/urls.py
+++ CompanyManager/employees/urls.py
@@ -6,5 +6,5 @@ app_name = 'employees'
 urlpatterns = [
     path('', views.employees, name = 'employees'),
     path('add/', views.addEmployee, name = 'addEmployee'),
-    path('employee/<int:employees_pk/>', views.viewEmployee, name = 'viewEmployee'),
+    path('employee/<int:employees_pk>', views.viewEmployee, name = 'viewEmployee'),
 ]
