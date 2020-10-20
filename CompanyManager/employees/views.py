9ec70bd078614f77fbd347611574f864ce52a87e
empty-works <pama.mark@gmail.com>
2020-10-20 16:56:17 +0200

    Added delete button to the template and properly connected it to views.

--- CompanyManager/employees/views.py
+++ CompanyManager/employees/views.py
@@ -32,8 +32,7 @@ def viewEmployee(request, employees_pk):
             return render(request, 'employees/addemployee.html', {'form': EmployeeForm(), 'error':'Bad data passed in. Try again.'})
 
 def deleteEmployee(request, employees_pk):
-    emp = get_object_or_404(Employee, pk = employees_pk, user = request.user)
+    emp = get_object_or_404(Employee, pk = employees_pk)
     if request.method == 'POST':
-        form = EmployeeForm(request.POST, instance = emp)
-        form.delete()
+        emp.delete()
         return redirect('employees:employees')
