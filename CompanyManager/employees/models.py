56b066fdeaaf0657dea43426e7580e6e13ace39a
empty-works <pama.mark@gmail.com>
2020-10-27 18:57:32 +0100

    Modified employee table to include more relevant fields.

--- CompanyManager/employees/models.py
+++ CompanyManager/employees/models.py
@@ -3,21 +3,29 @@ from django.contrib.auth.models import User
 
 # Employee types list for the employeeType choice field.
 EMP_TYPES = {
-        ("potential", "Potential"),
-        ("hourly", "Hourly"),
-        ("salaried", "Salaried"),
-        ("manager", "Manager"),
-        ("executive", "Executive"),
+    ("potential", "Potential"),
+    ("hourly", "Hourly"),
+    ("salaried", "Salaried"),
+    ("manager", "Manager"),
+    ("executive", "Executive"),
+}
+
+STATUS_TYPES = {
+    ("active", "Active"),
+    ("hiatus", "Hiatus"),
+    ("former", "Former"),
 }
 
 class Employee(models.Model):
     firstName = models.CharField(max_length = 200)
     lastName = models.CharField(max_length = 200)
+    position = models.CharField(max_length = 200)
     birthdate = models.DateField(blank = True)
     dateCreated = models.DateTimeField(auto_now_add = True, null = True) # Required so no blank=True
     dateHired = models.DateTimeField(null = True, blank = True)
     dateFired = models.DateTimeField(null = True, blank = True)
     wage = models.DecimalField(max_digits = 8, decimal_places = 2, blank = True, null = True) 
+    #TODO phone = models.
     employeeType = models.CharField(
             max_length = 20,
             choices = EMP_TYPES,
