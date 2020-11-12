fe2715f0179beef1baca9b841ced9e0c5a5d5fdf
empty-works <pama.mark@gmail.com>
2020-11-12 16:01:15 +0100

    Added 'back' button functionality to some templates. Will implement this to more templates later.

--- CompanyManager/CompanyManager/settings.py
+++ CompanyManager/CompanyManager/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # The following is needed for the 'back' button
+                'django.template.context_processors.request',
             ],
         },
     },
