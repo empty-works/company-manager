3c514f0b305f5b99033a7825b6ebb9a1a1586098
empty-works <pama.mark@gmail.com>
2020-12-04 20:00:38 +0100

    Created a unit test for the add employee form. Still incomplete.

--- CompanyManager/employees/tests.py
+++ CompanyManager/employees/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from CompanyManager.factories import UserFactory
+from CompanyManager.forms import EmployeeForm
 
-# Create your tests here.
+class EmployeeFormTest(TestCase):
+    def self(request):
+        self.user = UserFactory()
+        self.client.login(username=self.user.email, password='pass')
