db4eeeff551adee5330b724e765afb3229a6320a
empty-works <pama.mark@gmail.com>
2020-12-05 20:22:03 +0100

    Added the required form data in the employee form test.

--- CompanyManager/employees/tests.py
+++ CompanyManager/employees/tests.py
@@ -3,6 +3,22 @@ from CompanyManager.factories import UserFactory
 from CompanyManager.forms import EmployeeForm
 
 class EmployeeFormTest(TestCase):
-    def self(request):
+    def setup(self):
         self.user = UserFactory()
         self.client.login(username=self.user.email, password='pass')
+
+    def form_data(self, first, last, position, dateHired, wage, employeeType, address, email):
+        return EmployeeForm(
+                user = self.user,
+                data = {
+                    'first_name':first,
+                    'last_name':last,
+                    'position':position,
+                    'date_hired':dateHired,
+                    'wage':wage,
+                    'employee_type':employeeType,
+                    'address':address,
+                    'email':email,
+                    }
+                ) 
+
