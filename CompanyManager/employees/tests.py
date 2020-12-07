c1eb05707c1be2b13c642da616ae7703e43f3606
empty-works <pama.mark@gmail.com>
2020-12-07 19:14:59 +0100

    Added tests for the experience formset in the add employee form.

--- CompanyManager/employees/tests.py
+++ CompanyManager/employees/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from CompanyManager.factories import UserFactory
+from django.core.urlresolvers import reverse
 from CompanyManager.forms import EmployeeForm
 
+# Test basic add employee form
 class EmployeeFormTest(TestCase):
     def setup(self):
         self.user = UserFactory()
@@ -11,11 +13,11 @@ class EmployeeFormTest(TestCase):
         return EmployeeForm(
                 user = self.user,
                 data = {
-                    'first_name':first,
-                    'last_name':last,
+                    'firstName':first,
+                    'lastName':last,
                     'position':position,
                     'wage':wage,
-                    'employee_type':employeeType,
+                    'employeeType':employeeType,
                     'address':address,
                     'email':email,
                 }
@@ -27,13 +29,13 @@ class EmployeeFormTest(TestCase):
 
     def test_missing_first_name(self):
         form = self.form_data('', 'Last', 'Position', 'Wage', 'EmployeeType', 'Address', 'Email')
-        errors = form['first_name'].errors.as_data()
+        errors = form['firstName'].errors.as_data()
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0].code, 'required')
 
     def test_missing_last_name(self):
         form = self.form_data('First', '', 'Position', 'Wage', 'EmployeeType', 'Address', 'Email')
-        errors = form['last_name'].errors.as_data()
+        errors = form['lastName'].errors.as_data()
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0].code, 'required')
 
@@ -51,7 +53,7 @@ class EmployeeFormTest(TestCase):
 
     def test_missing_employee_type(self):
         form = self.form_data('First', 'Last', 'Position', 'Wage', '', 'Address', 'Email')
-        errors = form['employee_type'].errors.as_data()
+        errors = form['employeeType'].errors.as_data()
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0].code, 'required')
 
@@ -66,3 +68,110 @@ class EmployeeFormTest(TestCase):
         errors = form['email'].errors.as_data()
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0].code, 'required')
+
+# Test add employee formset for adding experience
+class ExperienceFormsetTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.client.login(username=self.user.email, password='pass')
+
+    def form_data(self, from_date, to_date, text):
+        return EmployeeForm(
+            user=self.user,
+            data={
+                'firstName': 'First',
+                'lastName': 'Last',
+                'position': 'Position',
+                'wage': 'Wage',
+                'employeeType': 'EmployeeType',
+                'address': 'Address',
+                'email': 'Email',
+                'form-TOTAL_FORMS': 1,
+                'form-INITIAL_FORMS': 0,
+                'form-0-from_date': fromDate,
+                'form-0-to_date': toDate,
+                'form-0-text': text,
+            }
+        )
+
+    def post_data(self, from_date1, to_date1, text1, from_date2='', to_date2='', text2=''):
+        return self.client.post(
+            reverse('test:profile-settings'),
+            data={
+                'form-TOTAL_FORMS': 2,
+                'form-INITIAL_FORMS': 0,
+                'form-0-from_date': from_date1,
+                'form-0-to_date': to_date1,
+                'form-0-text': text1,
+                'form-1-from_date': from_date2,
+                'form-1-to_date': to_date2,
+                'form-1-text': text2,
+            }
+        )
+
+    def raise_formset_error(self, response,error):
+        self.assertFormsetError(
+            response,
+            formset='exp_formset',
+            form_index=None,
+            field=None,
+            errors=error
+        )
+
+    def test_valid_data(self):
+        form = self.form_data('My Link', 'http://mylink.com')
+        self.assertTrue(form.is_valid())
+
+    def test_empty_fields(self):
+        """
+        Test validation passes when no data is provided
+        (data is not required).
+        """
+        form = self.form_data('', '', '')
+        self.assertTrue(form.is_valid())
+
+    def test_duplicate_from_date(self):
+        """
+        Test validation fails when an anchor is submitted more than once.
+        """
+        response = self.post_data('My Link', 'http://mylink.com',
+                                  'My Link', 'http://mylink2.com')
+
+        self.raise_formset_error(response,
+                                 'Links must have unique anchors and URLs.')
+
+    def test_duplicate_to_date(self):
+        """
+        Test validation fails when a URL is submitted more than once.
+        """
+        response = self.post_data('My Link', 'http://mylink.com',
+                                  'My Link2', 'http://mylink.com')
+
+        self.raise_formset_error(response,
+                                 'Links must have unique anchors and URLs.')
+
+    def test_duplicate_text(self):
+        """
+        Test validation fails when a URL is submitted more than once.
+        """
+        response = self.post_data('My Link', 'http://mylink.com',
+                                  'My Link2', 'http://mylink.com')
+
+        self.raise_formset_error(response,
+                                 'Links must have unique anchors and URLs.')
+
+    def test_anchor_without_url(self):
+        """
+        Test validation fails when a link is submitted without a URL.
+        """
+        response = self.post_data('My Link', '')
+
+        self.raise_formset_error(response, 'All links must have a URL.')
+
+    def test_url_without_anchor(self):
+        """
+        Test validation fails when a link is submitted without an anchor.
+        """
+        response = self.post_data('', 'http://mylink.com')
+
+        self.raise_formset_error(response, 'All links must have an anchor.')
