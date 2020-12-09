3c485a3618f1d4d78b360778d87fcadd0ab2ca6f
empty-works <pama.mark@gmail.com>
2020-12-09 21:23:39 +0100

    Added fake data and asserts for the employee form in the test forms script.

--- CompanyManager/employees/tests/test_views.py
+++ CompanyManager/employees/tests/test_views.py
@@ -1,7 +1,28 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from myapp.factories import UserFactory
-from myapp.models import User, UserLink
+from employees.factories import UserFactory
+from employees.models import Employee, Experience 
 
-class EmployeeFormsTest(TestCase):
+class AddEmployeeTest(TestCase):
+    def test_can_update_employee(self):
+        user = UserFactory()
+        self.client.login(username=user.email, password='pass')
+        response = self.client.post(
+                reverse('employees:addEmployee'),
+                data = {
+                       'firstName': 'New First Name',
+                       'lastName': 'New Last Name',
+                       'position': 'Lead Programmer'
+                       'wage': '1000000',
+                       'employeeType': 'Salaried',
+                       'address': '1100 Hobart Ave. Culvert City, CA',
+                       'email': 'hello_there@123.com',
+                    },
+                )
 
+        employee = Employee.objects.get(id=user.id)
+        experience = Experience.objects.get(employee=employee)
+
+        self.assertEqual(employee.firstName, 'New First Name')
+        self.assertEqual(employee.lastName, 'New Last Name')
+        
