1d6c0fae4a9c402b0137f76b0ad0104a43023d26
empty-works <pama.mark@gmail.com>
2020-12-09 19:32:13 +0100

    Created another test for the employee forms and formset.

---
+++ CompanyManager/employees/tests/test_views.py
@@ -0,0 +1,7 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from myapp.factories import UserFactory
+from myapp.models import User, UserLink
+
+class EmployeeFormsTest(TestCase):
+
