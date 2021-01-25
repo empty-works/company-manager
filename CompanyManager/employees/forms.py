59724ebf2e6c78a82a6f9dfdd45e1744c17de635
empty-works <pama.mark@gmail.com>
2021-01-25 08:18:06 +0100

    Updated the models script in the employees app to adhere to PEP8 standards.

--- CompanyManager/employees/forms.py
+++ CompanyManager/employees/forms.py
@@ -2,29 +2,29 @@ from django.forms import ModelForm
 from django import forms
 from django.forms import modelformset_factory
 from .models import Employee
-from .models import Experience 
+from .models import Experience
 from .models import Skill
 
 class EmployeeForm(ModelForm):
     class Meta:
         model = Employee
-        fields = ['firstName', 
-                'lastName', 
-                'position',
-                'birthDate', 
-                'dateHired', 
-                'dateFired', 
-                'wage', 
-                'emergencyContact',
-                'employeeType', 
-                'address', 
-                'email', 
-                'picture', 
-                'cur_assignment']
+        fields = ['firstName',
+                  'lastName',
+                  'position',
+                  'birthDate',
+                  'dateHired',
+                  'dateFired',
+                  'wage',
+                  'emergencyContact',
+                  'employeeType',
+                  'address',
+                  'email',
+                  'picture',
+                  'cur_assignment']
         widgets = {
-                'birthDate': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
-                'dateHired': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
-                'dateFired': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
+            'birthDate': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
+            'dateHired': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
+            'dateFired': forms.DateInput(attrs={'class':'form-control', 'type':'date'}),
         }
 
     def __init__(self, *args, **kwargs):
@@ -45,11 +45,11 @@ class ExperienceForm(ModelForm):
     class Meta:
         model = Experience
         fields = ['from_date',
-                  'to_date', 
+                  'to_date',
                   'text',]
-        # Using widgets to specify date type in the form fixed issue where it 
+        # Using widgets to specify date type in the form fixed issue where it
         # only showed up as a text field.
         widgets = {
-                'from_date':forms.DateInput(attrs={'type':'date'}),
-                'to_date':forms.DateInput(attrs={'type':'date'}),
+            'from_date':forms.DateInput(attrs={'type':'date'}),
+            'to_date':forms.DateInput(attrs={'type':'date'}),
                 }
