1780c3145135fb836564f95d459e57d8c5feda38
empty-works <pama.mark@gmail.com>
2021-02-01 07:55:40 +0100

    PEP8 refactoring for the models code in the employee app.

--- CompanyManager/employees/models.py
+++ CompanyManager/employees/models.py
@@ -24,59 +24,60 @@ SKILL_RANKS = {
 }
 
 class Employee(models.Model):
-    firstName = models.CharField(max_length = 200)
-    lastName = models.CharField(max_length = 200)
-    position = models.CharField(max_length = 200, blank = True)
-    birthDate = models.DateField(blank = True)
-    dateCreated = models.DateTimeField(auto_now_add = True, null = True) # Required so no blank=True
-    dateHired = models.DateTimeField(null = True, blank = True)
-    dateFired = models.DateTimeField(null = True, blank = True)
-    wage = models.DecimalField(max_digits = 8, decimal_places = 2, blank = True, null = True) 
-    emergencyContact = models.CharField(max_length = 400, blank = True)
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    position = models.CharField(max_length=200, blank=True)
+    birthDate = models.DateField(blank=True)
+    dateCreated = models.DateTimeField(auto_now_add=True, null=True) # Required so no blank=True
+    dateHired = models.DateTimeField(null=True, blank=True)
+    dateFired = models.DateTimeField(null=True, blank=True)
+    wage = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    emergencyContact = models.CharField(max_length=400, blank=True)
     #TODO phone = models.
     employeeType = models.CharField(
-            max_length = 20,
-            choices = EMP_TYPES,
-            default = 'potential',
-            )
-    address = models.CharField(max_length = 1000)
-    email = models.EmailField(blank = True)
-    picture = models.ImageField(upload_to = 'employees/images/', default = 'employees/images/240x240_default_img.jpg', blank = True)
-    cur_assignment = models.TextField(blank = True)
-    recorded_by = models.ForeignKey(User, default= None, null=True, on_delete=models.CASCADE)
-    
+        max_length=20,
+        choices=EMP_TYPES,
+        default='potential',
+        )
+    address = models.CharField(max_length=1000)
+    email = models.EmailField(blank=True)
+    picture = models.ImageField(upload_to='employees/images/',
+                                default='employees/images/240x240_default_img.jpg',
+                                blank=True)
+    cur_assignment = models.TextField(blank=True)
+    recorded_by = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
     # So the actual name of the object appears when looking at the database in Django Admin for example
     def __str__(self):
         return self.lastName + ", " + self.firstName
 
 class Experience(models.Model):
     employee = models.ForeignKey(
-            Employee,
-            on_delete = models.CASCADE,
-            null = True,
-            )
-    from_date = models.DateField(null = True)
-    to_date = models.DateField(null = True)
-    text = models.TextField(default = "INSIDE EXPERIENCE TEXT", blank = True)
+        Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        )
+    from_date = models.DateField(null=True)
+    to_date = models.DateField(null=True)
+    text = models.TextField(default="INSIDE EXPERIENCE TEXT", blank=True)
 
 class Skill(models.Model):
     employee = models.ForeignKey(
-            Employee,
-            on_delete = models.CASCADE,
-            null = True,
-            ) 
-    name = models.CharField(max_length = 200, blank = True)
+        Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        )
+    name = models.CharField(max_length=200, blank=True)
     rank = models.CharField(
-            max_length = 50,
-            choices = SKILL_RANKS,
-            default = 'novice',
-            ) 
+        max_length=50,
+        choices=SKILL_RANKS,
+        default='novice',
+        )
 
 class Assignment(models.Model):
     employee = models.ForeignKey(
-            Employee,
-            on_delete = models.CASCADE,
-            null = True,
-            )
-    title = models.CharField(max_length = 300, blank = True)
-    description = models.TextField(default = "ASSIGNMENT DESCRIPTION", blank = True)
+        Employee,
+        on_delete=models.CASCADE,
+        null=True,
+        )
+    title = models.CharField(max_length=300, blank=True)
+    description = models.TextField(default="ASSIGNMENT DESCRIPTION", blank=True)
